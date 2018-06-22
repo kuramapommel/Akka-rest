2a8d517a23048a310f9ded6758621d3672d96d6f
kuramapommel <pommel.pg@gmail.com>
2018-06-22 12:38:49 +0900

    不要な継承を削除

--- src/main/scala/com/tiloom6/RestApi.scala
+++ src/main/scala/com/tiloom6/RestApi.scala
@@ -1,12 +1,14 @@
 package com.tiloom6
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
 
 /**
-  * REST-APIのルーティングを管理するトレイト
+  * REST-APIトレイト
   */
-trait RestApi extends StartUp {
+trait RestApi extends RequestTimeout {
 
   /** REST-APIルータ */
   protected val routes: Route
@@ -15,11 +17,35 @@ trait RestApi extends StartUp {
   protected implicit val actorSystem: ActorSystem
 
   /**
-    * REST-APIサーバを起動する
+    * サーバ起動
     */
   def start() = {
-    startUp(routes)
+    val host = actorSystem.settings.config.getString("http.host")
+    val port = actorSystem.settings.config.getInt("http.port")
+    startHttpServer(routes, host, port)
   }
+
+  /**
+    * 指定されたhostとpostでサーバを起動する
+    *
+    * @param api REST-APIルータ
+    * @param host host ip
+    * @param port 起動するポート番号
+    */
+  private def startHttpServer(api: Route, host: String, port: Int) = {
+    import scala.io._
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    implicit val materializer = ActorMaterializer()
+
+    // サーバ起動
+    val bindingFuture = Http().bindAndHandle(api, host, port)
+
+    println(s"Server online at host = ${host}, port = ${port}. Press RETURN to stop...")
+
+    // enter押下でサーバ停止
+    StdIn.readLine()
+    bindingFuture.flatMap(_.unbind()).onComplete(_ => actorSystem.terminate())
+  }
+
 }
-
-
