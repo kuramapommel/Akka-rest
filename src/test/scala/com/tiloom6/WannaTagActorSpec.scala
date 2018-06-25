78f4e3d26bc0575ded032023d35a63e82c98aea7
kuramapommel <pommel.pg@gmail.com>
2018-06-25 19:16:22 +0900

    テスト修正

--- src/test/scala/com/tiloom6/WannaTagActorSpec.scala
+++ src/test/scala/com/tiloom6/WannaTagActorSpec.scala
@@ -2,9 +2,14 @@ package com.tiloom6
 
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
+import org.joda.time.DateTime
 
 import scala.concurrent.duration._
 import org.scalatest._
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Success, Try}
 
 class WannaTagActorSpec extends TestKit(ActorSystem("testBoxOffice"))
   with WordSpecLike // BDDスタイルのテストを実現
@@ -12,9 +17,11 @@ class WannaTagActorSpec extends TestKit(ActorSystem("testBoxOffice"))
   with ImplicitSender // Actor側でsenderにメッセージを返している場合にそのメッセージを受け取る
   with StopSystemAfterAll // テスト後にActorSystemを停止する
   {
-  import WannaTagActor._
 
   "A WannaTag Actor" must {
+    import WannaTagActor._
+    import com.tiloom6.Tables._
+
     "return wannaTag when it receives a GetWannaTag message" in {
       import akka.util.Timeout
 
@@ -22,9 +29,31 @@ class WannaTagActorSpec extends TestKit(ActorSystem("testBoxOffice"))
       implicit val timeout: Timeout = FiniteDuration(duration.length, duration.unit)
       val wannatagActor = system.actorOf(WannaTagActor.props, "wannatag")
 
+      // テスト実行
       wannatagActor ! GetWannaTags("older", 1, 20)
-      Thread.sleep(2000)
-      expectMsg(scala.concurrent.Future.successful(0))
+
+      // アサート
+      expectMsgPF() {
+        case futureResult: Future[Any] =>
+
+          // Actorからの受け取り結果をFuture[Seq[WannatagRow]]に変換
+          val futureWannatags = futureResult.mapTo[Seq[WannatagRow]]
+
+          // futureの処理待ち
+          Await.ready(futureWannatags, duration)
+
+          // Futur[Try[Seq[WannatagRow]]] -> WannatagRow 途中で例外が発生したらテストエラーになるだけなのでgetでOK
+          val tryWannatags = futureWannatags.value.get
+          val wannatags = tryWannatags.get
+          val wannatag = wannatags.headOption.get
+
+          // アサーション
+          wannatag.title must be("aaa")
+          wannatag.body must be("bbb")
+          wannatag.username must be("ccc")
+
+        case _ => fail()
+      }
     }
   }
 }
